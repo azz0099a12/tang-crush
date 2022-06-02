7e51f88a33ce25653880ec5a740ad7c5b81a792d
azz0099a12 <85283927+azz0099a12@users.noreply.github.com>
2022-06-03 01:52:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Hương à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    Đằng sau chữ yêu đây là thương!!!`,
+    btnIntro: 'Làm người yêu Duy nhé !!!!!',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnYes: 'Chỉ có thể là yêu Duy thôi :))))',
+    btnNo: 'Còn lâu :3',
+    question:'Giữa 7 tỉ người trên thế giới tại sao Hương lại chọn Duy vậy!!!',
+    btnReply: 'Gửi cho anh ❤',
+    reply: 'Ừa .....thì emm thương anhhh :333  ',
+    mess: 'Anh biết mà :)) . Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Hôm sau ra Đà Nẵng anh sang đón đi chơi nhá!!!!!',
+    btnAccept: 'Okiiiii lun ❤',
+    messLink: 'https://vt.tiktok.com/ZSdgx292G/?k=1' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
